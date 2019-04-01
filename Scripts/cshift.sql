799e8abd86657313b20720c491f15de9b2b7d41e
Siew Wei Lun <weilun.siew.2017@sis.smu.edu.sg>
2019-04-01 21:39:38 +0800

    push cshfits sql, shifts xsd, tibco shifts

--- Scripts/cshift.sql
+++ Scripts/cshift.sql
@@ -1,13 +1,11 @@
-CREATE DATABASE IF NOT EXISTS `pshift` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `pshift`;
+CREATE DATABASE IF NOT EXISTS `cshift` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `cshift`;
 
-DROP TABLE IF EXISTS `pshift`;
-CREATE TABLE `pshift` (
+CREATE TABLE `cshift` (
   `shift_id` INT(11) NOT NULL auto_increment,
   `employee_id` INT(11),
   `Period` varchar(10) NOT NULL,
   `PDay` varchar(10) NOT NULL,
   `Timing` varchar(10) NOT NULL,
-  `Status` varchar(10) NOT NULL,
   CONSTRAINT pshift_pk PRIMARY KEY (shift_id)
 );

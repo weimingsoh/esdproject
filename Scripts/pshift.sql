96313614ae168833d70d61564ed05e79b444deb5
Jazreel <jazreel.tho.2017@sis.smu.edu.sg>
2019-04-04 06:48:47 +0800

    almost final sql scripts

--- Scripts/pshift.sql
+++ Scripts/pshift.sql
@@ -3,11 +3,11 @@ USE `pshift`;
 
 DROP TABLE IF EXISTS `pshift`;
 CREATE TABLE `pshift` (
-  `shift_id` INT(11) NOT NULL auto_increment,
-  `employee_id` INT(11),
+  `ID` INT(11) NOT NULL auto_increment,
+  `EmployeeID` INT(11),
   `Period` varchar(10) NOT NULL,
   `PDay` varchar(10) NOT NULL,
   `Timing` varchar(10) NOT NULL,
   `Status` varchar(10) NOT NULL,
-  CONSTRAINT pshift_pk PRIMARY KEY (shift_id)
+  CONSTRAINT pshift_pk PRIMARY KEY (ID)
 );
